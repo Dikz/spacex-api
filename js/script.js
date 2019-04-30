9dbfc7004d57c47ac7fb02771a9fce57fa06cbae
Dikz <cicerogabriel12@hotmail.com>
2019-04-30 16:55:03 -0300

    Numero do lançamento

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const cardLaunch = data => data.map(launch => `
 	<li class="media launch-item m-4">
 		<img src="${launch.links.mission_patch_small ? launch.links.mission_patch_small : './src/img/elon.png'}" class="mr-3" alt="..." style="width: 64px;">
 		<div class="media-body">
-			<h5 class="mt-0 mb-1">${launch.mission_name}</h5>
+			<h5 class="mt-0 mb-1"><span class="badge badge-dark">${launch.flight_number}</span> ${launch.mission_name}</h5>
 			${launch.details ? launch.details.substring(0, 40) : ''}...
 			<div>
 				<p><span class="badge badge-danger">Rocket: ${launch.rocket.rocket_name}</span></p>
@@ -29,7 +29,7 @@ const cardLaunch = data => data.map(launch => `
 const cardUpcoming = data => data.map(launch => `
 	<div class="media mb-4 ml-5 m-4 text-light">
 		<div class="media-body">
-			<h5 class="mt-0 mb-1">${launch.mission_name}</h5>
+			<h5 class="mt-0 mb-1"><span class="badge badge-light">${launch.flight_number}</span> ${launch.mission_name}</h5>
 			${launch.details ? launch.details.substring(0, 40) : 'No details yet'}...
 			<div>
 				<p class="badge badge-info">${launch.launch_year}</p>
