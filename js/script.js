1d7596d211565208fa0e86842e371d303b7e2392
Dikz <cicerogabriel12@hotmail.com>
2019-04-30 04:02:41 -0300

    Lista de lançamentos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 $(async () => {
 	const launches = await fetch("https://api.spacexdata.com/v3/launches/").then(r => r.json())
 
-	// $("id or class div").append(cardJhon(launches))
+	$("#launches").append(cardLaunch(launches))
+	removeLoading('#loading')
 })
 
-const cardJhon = data => data.map(launch => `
-	<p>${launch.mission_name}</p>
+const cardLaunch = data => data.map(launch => `
+	<li class="media launch-item m-4">
+		<img src="${launch.links.mission_patch_small ? launch.links.mission_patch_small : './src/img/elon.png'}" class="mr-3" alt="..." style="width: 64px;">
+		<div class="media-body">
+			<h5 class="mt-0 mb-1">${launch.mission_name}</h5>
+			${launch.details ? launch.details.substring(0, 40) : ''}...
+			<div>
+				<p class="badge badge-info">${launch.launch_year}</p>
+			</div>
+		</div>
+	</li>
 `)
+
+const removeLoading = (selector) => {
+	$(selector).hide()
+}
